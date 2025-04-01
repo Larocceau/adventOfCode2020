6cd3b285e69b2dddfcbae046cc4cd7dc84e22f3d
Joost Kaptein <joost@compositional-it.com>
2025-04-01 20:35:08 +0100

    day 10 part 2

--- day10/src/lib.rs
+++ day10/src/lib.rs
@@ -1,10 +1,12 @@
+use std::{cell::RefCell, collections::HashMap, rc::Rc};
 
-fn jolt_differences(chargers: &[u32]) -> Vec<u32>{
+
+fn jolt_differences(chargers: &[usize]) -> Vec<usize>{
     let mut data = chargers.to_vec();
     data.sort();
 
     let first = data[0];
-    let mut res: Vec::<u32> = data.windows(2).map(|v| v[1] - v[0]).collect();
+    let mut res: Vec::<usize> = data.windows(2).map(|v| v[1] - v[0]).collect();
 
     res.push(first);
     res.push(3);
@@ -12,6 +14,56 @@ fn jolt_differences(chargers: &[u32]) -> Vec<u32>{
 }
 
 
+
+/// # Examples
+///
+/// ```
+/// use day10::solve_part_2;
+/// let data = &[28, 33, 18, 42, 31, 14, 46, 20, 48, 47, 24, 23, 49, 45, 19, 38, 39, 11, 1, 32, 25, 35, 8, 17, 7, 9, 4, 2, 34, 10, 3,];
+/// let res = solve_part_2(data);
+/// assert_eq!(res, 19208);
+/// ```
+pub fn solve_part_2(chargers: &[usize]) -> usize {
+    no_of_routes(chargers, &0, &(chargers.iter().max().unwrap() + 3))
+}
+
+
+
+fn no_of_routes(chargers: &[usize],start_value: &usize, target_value: &usize) -> usize {
+    let memo = Rc::new(RefCell::new(HashMap::new()));
+
+    no_of_routes_memo(chargers, start_value, target_value, memo)
+}
+
+
+fn no_of_routes_memo(chargers: &[usize], start_value: &usize, target_value: &usize, memo: Rc<RefCell<HashMap<(Vec<usize>, usize, usize), usize>>>) -> usize {
+    let key = (chargers.to_vec(), *start_value, *target_value);
+    if let Some(res) = memo.borrow().get(&key){
+        *res
+    } else {
+        let next: usize= 
+        chargers.iter()
+        .filter(|v| (**v - start_value) <= 3)
+        .map(|v|  {
+            
+            let chargers: Vec<usize> = chargers.iter().filter(|i| *i > v).cloned().collect();
+            let memo = memo.clone();
+    
+            no_of_routes_memo(&chargers, v, target_value, memo)
+    
+        }
+        
+        ).sum();
+        let can_finish = target_value - start_value <= 3;
+    
+        let res = next + (if can_finish {1} else {0});
+
+        memo.borrow_mut().insert(key, res);
+        res
+
+    }
+    
+}
 /// # Examples
 ///
 /// ```
@@ -20,7 +72,7 @@ fn jolt_differences(chargers: &[u32]) -> Vec<u32>{
 /// let res = solve_part_1(data);
 /// assert_eq!(res, 220)
 /// ```
-pub fn solve_part_1(data: &[u32]) -> u32 {
+pub fn solve_part_1(data: &[usize]) -> u32 {
 
     let jolt_differences = jolt_differences(data);
     let frequencies = util::count_by(&jolt_differences, |&v| v);
--- day10/src/main.rs
+++ day10/src/main.rs
@@ -4,13 +4,17 @@ use day10::solve_part_1;
 
 
 fn main() -> Result<(),Error>{
-    let data: Vec<u32> = util::load_data()?
+    let data: Vec<usize> = util::load_data()?
         .split_ascii_whitespace()
-        .map(|v| v.parse::<u32>().unwrap())
+        .map(|v| v.parse::<usize>().unwrap())
         .collect();
 
     let part1_solution = solve_part_1(&data);
     println!("the solution for part 1 is {part1_solution}");
+
+    let part_2_solution = day10::solve_part_2(&data);
+
+    println!("solution for part 2 is {part_2_solution}");
 
 
     Ok(())
